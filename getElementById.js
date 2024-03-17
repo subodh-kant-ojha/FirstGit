8f6b21263374fbffc05f6951aa589fb33e825f4d
subodh-kant-ojha <subodhkantojha2001@gmail.com>
2024-03-17 16:11:42 +0530

    getElementById

---
+++ getElementById.js
@@ -0,0 +1,14 @@
+const mainHeading=document.getElementById('main-heading');
+mainHeading.textContent='Fruit World';
+mainHeading.style.color='orange'
+
+const header=document.getElementById('header');
+
+header.style.backgroundColor='green';
+header.style.borderBottom='solid orange';
+
+const heading=document.getElementById('basket-heading');
+heading.style.color='green';
+
+const thanks= document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
